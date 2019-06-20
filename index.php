2c7cb1dfe50a6c6be5875595dab37ddc677e3f3b
Bas Brands <bas@moodle.com>
2019-06-20 12:46:33 +0200

    tweaks and stuff

--- index.php
+++ index.php
@@ -26,60 +26,68 @@ require_once(__DIR__ . '/../../config.php');
 require_login(1);
 
 // For the messaging template
-$gray = optional_param('gray', '', PARAM_TEXT);
-$primary = optional_param('primary', '', PARAM_TEXT);
-$parsed = optional_param('parsed', 0, PARAM_INT);
-
 $template = optional_param('template', 'messaging', PARAM_TEXT);
-
-$scss = $CFG->dirroot . '/local/uitest/sass/' . $template . '.scss';
+$parsed = optional_param('parsed', 1, PARAM_INT);
 
 $preset = file_get_contents($CFG->dirroot . '/local/uitest/sass/variables.scss');
 
-if (!empty($gray)) {
-	$preset .= "\n" . '$secondary: $' . $gray . ";\n";
+if (!empty($_POST)) {
+	$preset .= "\n" . $_POST['variable'] . ': ' .  $_POST['value'] . ";\n";
 }
 
-if (!empty($primary)) {
-	$preset .= "\n" . '$nabvar-bg-color: ' . $primary . ";\n";
-}
+$scss = $CFG->dirroot . '/local/uitest/sass/' . $template . '.scss';
 
 $compiler = new core_scss();
 $compiler->prepend_raw_scss($preset);
 $compiler->set_file($scss);
 $css = $compiler->to_css();
+
+$renderable = (object)[];
+$renderable->inputcss = $preset . "\n" . file_get_contents($scss);
+$renderable->outputcss = $css;
+if (!empty($_POST)) {
+	if (isset($_POST['variable'])) {
+		$renderable->variable = $_POST['variable'];
+	}
+	if (isset($_POST['value'])) {
+		$renderable->value = $_POST['value'];
+	}
+}
+
 if ($parsed) {
 	$parser = new core_cssparser($css);
 	$csstree = $parser->parse();
 }
+if ($parsed) {
+	$renderable->parsed = $csstree;
+}
 
-$renderable = (object)[];
-$renderable->inputcss = $scss;
-$renderable->outputcss = $parsed ? $csstree : $css;
+$url = new moodle_url('/local/uitest/index.php');
+$url->param('template', $template);
 
-$PAGE->set_url(new moodle_url('/local/uitest/index.php'));
+$renderable->baseurl = $url;
+
+$PAGE->set_url($url);
 $PAGE->set_pagelayout('incourse');
 $PAGE->set_title('Sass Testing');
 $PAGE->set_heading('Sass Testing');
 $PAGE->navbar->add('Sass Testing');
 
-$templates = ['messaging' => 'messaging', 'navbar' => 'navbar', 'sass' => 'sass'];
+$templates = [
+	'messaging' => 'messaging',
+	'navbar' => 'navbar',
+	'sass' => 'sass',
+	'example' => 'example',
+	'toast' => 'toast'
+];
 echo $OUTPUT->header();
 
 $s = new single_select(new moodle_url('/local/uitest/index.php'), 'template', $templates, $template, null);
 $s->label = 'templates';
 $s->class = 'templates';
 
-// set_config('coursecolor1', '#000', 'core_admin');
+$renderable->chooser = $OUTPUT->render($s);
 
-// $configurablecolors = range(1, 10);
-// foreach ($configurablecolors as $color)  {
-// 	echo $color . ':' . $OUTPUT->get_generated_color_for_id($color) . "<br>";
-// }
-
-echo $OUTPUT->render_from_template('local_uitest/intro', (object)[]);
-
-echo $OUTPUT->render($s);
 echo $OUTPUT->render_from_template('local_uitest/' .$template, $renderable);
 
 echo $OUTPUT->footer();
